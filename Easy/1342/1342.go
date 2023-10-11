584cb950daf5d9236db7fe8555a0e6a81a1c5cab
Kevin Collins <kevinmscollins@gmail.com>
2023-10-11 20:19:52 +0100

    1342

---
+++ Easy/1342/1342.go
@@ -0,0 +1,20 @@
+func numberOfSteps(num int) int {
+	if num == 0 {
+		return 0
+	}
+
+	result := 0
+	for num > 0 {
+        //bitwise & operator
+		if num&1 == 1 {
+			result += 2
+		} else {
+			result += 1
+		}
+        
+        //bitwise shift
+		num >>= 1
+	}
+
+	return result - 1
+}
---
+++ Easy/1342/README.md
@@ -0,0 +1,9 @@
+Runtime
+1ms
+Beats 72.35%of users with Go
+
+Memory
+1.92MB
+Beats 99.79%of users with Go
+
+Looking to use more bitwise work. Need to research here. Wouldn't have thought to use this before solution.
