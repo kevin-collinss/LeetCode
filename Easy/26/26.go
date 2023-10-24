5cad8a24c8d673b2632889f0c14eee99c77e51d8
Kevin Collins <kevinmscollins@gmail.com>
2023-10-24 10:06:12 +0100

    26

---
+++ Easy/26/26.go
@@ -0,0 +1,14 @@
+package easy
+
+func removeDuplicates(nums []int) int {
+	count := 0
+
+	for i := range nums {
+		if nums[i] != nums[count] {
+			count += 1
+			nums[count] = nums[i]
+		}
+	}
+
+	return count + 1
+}
