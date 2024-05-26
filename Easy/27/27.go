2a4e1d42909a344322da2f93db5772e2cb9c31ff
Kevin Collins <yournewemail@example.com>
2024-05-26 15:22:52 +0100

    27

--- Easy/27/27.go
+++ Easy/27/27.go
@@ -1,1 +1,18 @@
 package Easy
+
+func removeElement(nums []int, val int) int {
+
+	last := len(nums) - 1
+	i := 0
+
+	for i <= last {
+		if nums[i] == val {
+			nums[i] = nums[last]
+			nums[last] = 0
+			last--
+		} else {
+			i++
+		}
+	}
+	return i
+}
