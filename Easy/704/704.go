6d519dfe037f9adc3621dd40b89f367075b6e32e
Kevin Collins <kevinmscollins@gmail.com>
2024-05-27 17:28:46 +0100

    704

---
+++ Easy/704/704.go
@@ -0,0 +1,22 @@
+package Easy
+
+func search(nums []int, target int) int {
+
+	low := 0
+	high := len(nums)
+
+	for low < high {
+
+		med := low + (high-low)/2
+		val := nums[med]
+
+		if val == target {
+			return med
+		} else if val > target {
+			high = med
+		} else {
+			low = med + 1
+		}
+	}
+	return -1
+}
