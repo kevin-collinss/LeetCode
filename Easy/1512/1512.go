398d9c358a46f264dbef771a01697d2f6dbdd7a1
Kevin Collins <kevinmscollins@gmail.com>
2024-06-06 19:07:44 +0100

    1512

--- Easy/1512/1512.go
+++ Easy/1512/1512.go
@@ -1,2 +1,14 @@
 package Easy
 
+func numIdenticalPairs(nums []int) int {
+
+	count := 0
+	for i, val := range nums {
+		for j, valInner := range nums {
+			if val == valInner && j > i {
+				count++
+			}
+		}
+	}
+	return count
+}
