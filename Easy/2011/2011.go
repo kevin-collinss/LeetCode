6bcd8eb884b09db51e783d89edf6968b5037d420
Kevin Collins <kevinmscollins@gmail.com>
2024-06-07 22:14:26 +0100

    2011

--- Easy/2011/2011.go
+++ Easy/2011/2011.go
@@ -1,2 +1,15 @@
 package Easy
 
+func finalValueAfterOperations(operations []string) int {
+
+	X := 0
+	for _, op := range operations {
+
+		if op == "++X" || op == "X++" {
+			X++
+		} else {
+			X--
+		}
+	}
+	return X
+}
