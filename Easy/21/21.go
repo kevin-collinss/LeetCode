7a9e80bde8c9afe0de56b79efc8de6be3b973e6d
Kevin Collins <kevinmscollins@gmail.com>
2024-06-23 17:20:23 +0100

    21

--- Easy/21/21.go
+++ Easy/21/21.go
@@ -1,9 +1,31 @@
 package Easy
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+
+	mergeList := &ListNode{}
+	current := mergeList
+
+	for list1 != nil || list2 != nil {
+		if list1 == nil {
+			current.Next = list2
+			list2 = list2.Next
+		} else if list2 == nil {
+			current.Next = list1
+			list1 = list1.Next
+		} else if list1.Val >= list2.Val {
+			current.Next = list2
+			list2 = list2.Next
+		} else {
+			current.Next = list1
+			list1 = list1.Next
+		}
+		current = current.Next
+	}
+
+	return mergeList.Next
+}
